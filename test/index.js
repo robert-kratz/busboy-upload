5119ce48fe35036d42cf11d48215ebc0623499dc
Robert J. Kratz [MAC] <robert.kratz03@gmail.com>
2022-06-22 12:43:30 +0200

    added file upload test support

---
+++ test/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const busboy = require('connect-busboy')
+
+const app = express();
+
+app.use(busboy({ immediate: true }));
+
+const fileUpload = require('..');
+
+app.use('/upload', (req, res) => {
+
+    /**
+     * Is fired when file has been successfully uploaded
+     * @param {Object} fileInfo 
+     */
+    const success = (fileInfo) => {
+        res.status(200).json(fileInfo);
+    }
+
+    const config = {
+        uploadPath: __dirname + '/uploads/',
+        uploadName: (fieldName, filename, encoding, mimeType) => {
+            return fieldName + '-' + filename + '-' + Date.now();
+        },
+        mimeTypes: ['image/jpeg'],
+        maxsize: 100000,
+    }
+
+    fileUpload(req, success, config);
+})
+
+app.listen(443, () => console.log(`Started on http://127.0.0.1:443 ` + __filename))
